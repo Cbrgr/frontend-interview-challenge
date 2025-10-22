69825adefcd47077dce4ebeb9cb3985be7e36f4b
Constantin <constantin.brgr@gmail.com>
2025-10-22 22:14:05 +0200

    feat: setup the table layout

--- src/components/EmployeesTable.vue
+++ src/components/EmployeesTable.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>Fixme !</div>
+  <div class="w-[1280px] mx-auto pt-24">
+    <div class="w-full flex flex-col gap-16">
+      <div class="px-24">
+        <select>
+          <option>Active employees</option>
+        </select>
+      </div>
+      <div class="flex flex-col gap-8">
+        <div class="flex gap-20 px-24 h-40 items-center">
+          <div class="flex-1">
+            <p>Name</p>
+          </div>
+          <div class="flex-1 flex justify-end">
+            <p>Status</p>
+          </div>
+        </div>
+        <div class="flex gap-20 px-24 h-40 items-center">
+          <div class="flex-1">
+            <p>Michael J. Samuelson</p>
+          </div>
+          <div class="flex-1 flex justify-end">
+            <p>Active</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
